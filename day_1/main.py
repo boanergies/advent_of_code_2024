0470870bd8a18e87661e7b7c319c55257ed7b15b
Sean McElwain <semcelwa@gmail.com>
2024-12-03 21:26:03 -0600

    improve code

--- day_1/main.py
+++ day_1/main.py
@@ -12,17 +12,12 @@ def main(
     list_2 = list()
     with input_data.open() as fin:
         for line in fin:
-            num1, num2 = line.strip().split("   ")
-            list_1.append(int(num1))
-            list_2.append(int(num2))
+            num1, num2 = map(int, line.split())
+            list_1.append(num1)
+            list_2.append(num2)
     list_1.sort()
     list_2.sort()
-    print(list_1)
-    print(list_2)
-    distance = 0
-    for value1, value2 in zip(list_1, list_2):
-        distance += abs(value1 - value2)
-    print(distance)
+    print(sum([abs(a - b) for a, b in zip(list_1, list_2)]))
 
 
 if __name__ == "__main__":
